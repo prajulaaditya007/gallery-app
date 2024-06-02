dfba175b3d51adff3bc4100908579cff97513eb2
Prajul <prajulaadityaa@gmail.com>
2024-06-02 17:09:23 +0530

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 import Link from "next/link";
+import jsonData from "../utils/rows.json";
+
+interface Item {
+  name: string;
+  key: string;
+  customId: string | null;
+  url: string;
+  size: number;
+  uploadedAt: string;
+}
+
+const imageUrls = (jsonData: Item[]) => {
+  const mockUrls: string[] = jsonData.map((item) => item.url);
+  return mockUrls;
+};
+const mockUrls = imageUrls(jsonData);
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
