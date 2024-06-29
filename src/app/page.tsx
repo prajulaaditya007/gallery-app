aeb068b3e207954fe6ee59fa279a9c9be90a32c6
Prajul <prajulaadityaa@gmail.com>
2024-06-30 04:04:45 +0530

    clerk auth added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ export default async function HomePage() {
         orderBy: (model, {desc}) => desc(model.id)
     });
 
-    console.log(images);
-
     return (
         <main className="">
             <div className="flex flex-wrap gap-4 ">
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,14 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import {sql} from "drizzle-orm";
-import {index, pgTableCreator, serial, timestamp, varchar,} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import {
+  index,
+  pgTableCreator,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -13,17 +19,17 @@ import {index, pgTableCreator, serial, timestamp, varchar,} from "drizzle-orm/pg
 export const createTable = pgTableCreator((name) => `t3app-gallery_${name}`);
 
 export const images = createTable(
-    "image",
-    {
-        id: serial("id").primaryKey(),
-        name: varchar("name", {length: 256}).notNull(),
-        url: varchar("url", {length: 1024}).notNull(),
-        createdAt: timestamp("created_at", {withTimezone: true})
-            .default(sql`CURRENT_TIMESTAMP`)
-            .notNull(),
-        updatedAt: timestamp("updatedAt", {withTimezone: true}),
-    },
-    (example) => ({
-        nameIndex: index("name_idx").on(example.name),
-    })
+  "image",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt", { withTimezone: true }),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.name),
+  }),
 );
