785fc5de88f79d59798767740baceeb50f378822
Prajul <prajulaadityaa@gmail.com>
2024-06-30 04:35:25 +0530

    middleware edge function

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,1 +1,19 @@
-import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";function TopNav() {    return (        <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">            <div>Gallery</div>            <div>                <SignedOut>                    <SignInButton/>                </SignedOut>                <SignedIn>                    <UserButton/>                </SignedIn>            </div>        </nav>    );}export default TopNav;
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div> Wall Gallery</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,7 @@
-// middleware.tsimport { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);export default clerkMiddleware((auth, request) => {    if (isProtectedRoute(request)) auth().protect();});export const config = {    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],};
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
