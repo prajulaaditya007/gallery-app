5363408425a2a2f9816dcf3f5dcb617000c185c9
Prajul <prajulaadityaa@gmail.com>
2024-07-01 01:27:22 +0530

    user specific gallery

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/app/utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div> Wall Gallery</div>
-      <div>
+      <div>Wall Gallery</div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />{" "}
+          {/* imageUploader came from api/uploadthing/core.ts */}
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,34 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 async function Images() {
-    const images = await db.query.images.findMany({
-        orderBy: (model, { desc }) => desc(model.id)
-    });
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-    return (
-        <div className="flex flex-wrap gap-4 ">
-            {images.map((image, index) => (
-                <div key={image.id + "-" + index} className="w-48 flex flex-col">
-                    <img src={image.url} alt="image" />
-                    <div>{image.name}</div>
-                </div>
-            ))}
+  return (
+    <div className="flex flex-wrap gap-4 ">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default async function HomePage() {
-    return (
-        <main className="">
-            <SignedOut>
-                <div className="w-full h-full text-2xl text-center">Please sign in</div>
-            </SignedOut>
-            <SignedIn>
-                <Images />
-            </SignedIn>
-        </main>
-    );
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
 }
