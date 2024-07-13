5817913df132c44ea40c792d65d75ab69af1411e
Prajul <prajulaadityaa@gmail.com>
2024-07-14 02:56:00 +0530

    more clean up

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,18 +3,29 @@ import { getImage } from "~/server/queries";
 /**
  * Asynchronously renders the photo modal based on the provided photo ID.
  *
- * @param {object} params - Object containing the photo ID.
  * @param {string} params.id - The ID of the photo to be displayed.
  * @return {JSX.Element} The rendered photo modal.
+ * @param props
  */
-export default async function FullPageImageView(props: { id: number }) {
+export default async function FullPageImageView(props: {
+  id: number;
+}): Promise<JSX.Element> {
   // Retrieve the image data based on the ID
   const image = await getImage(props.id);
 
   // Render the modal with the image
   return (
-    <div className="flex w-full h-full bg-green-500">
-      <img src={image.url} className="w-96 object-contain" alt={image.name} />
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center ">
+        <img
+          src={image.url}
+          className="flex-shrink object-contain"
+          alt={image.name}
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
     </div>
   );
 }
